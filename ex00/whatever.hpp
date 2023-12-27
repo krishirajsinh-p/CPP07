c48d4bff7bf99c471544cec044351c65faac9f90
kpuwar <kpuwar@student.42heilbronn.de>
2023-12-28 00:36:18 +0100

    some formatting changes

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,30 +6,24 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 17:32:25 by kpuwar            #+#    #+#             */
-/*   Updated: 2023/12/27 17:36:40 by kpuwar           ###   ########.fr       */
+/*   Updated: 2023/12/28 00:34:43 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template <typename T>
-void swap(T &a, T &b)
-{
+template <typename T> void swap(T &a, T &b) {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
-template <typename T>
-T min(T a, T b)
-{
+template <typename T> T min(T a, T b) {
 	return (b < a) ? b : a;
 }
 
-template <typename T>
-T max(T a, T b)
-{
+template <typename T> T max(T a, T b) {
 	return (a < b) ? b : a;
 }
 
--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 17:42:56 by kpuwar            #+#    #+#             */
-/*   Updated: 2023/12/27 17:54:00 by kpuwar           ###   ########.fr       */
+/*   Updated: 2023/12/28 00:35:29 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,13 @@
 using std::cout;
 using std::endl;
 
-template<typename T>
-void iter(T* array, size_t length, void (*func)(T&)) {
-    for (size_t i = 0; i < length; ++i) {
-        func(array[i]);
-    }
+template<typename T> void iter(T* array, size_t length, void (*func)(T&)) {
+	for (size_t i = 0; i < length; ++i)
+		func(array[i]);
 }
 
-template<typename T>
-void printElement(T& element) {
-    cout << "[" << element << "] ";
+template<typename T> void printElement(T& element) {
+	cout << "[" << element << "] ";
 }
 
 #endif
